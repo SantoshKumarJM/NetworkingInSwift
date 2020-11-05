4f35a9038f6898fbd46e870cb18d5a242100b8b6
SantoshJM <santosh@orchids.edu.in>
2020-11-05 12:50:31 +0530

    Initial Commit

---
+++ ListView-SwiftUI/ListView_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListView_SwiftUIApp.swift
+//  ListView-SwiftUI
+//
+//  Created by Santosh on 05/11/20.
+//
+
+import SwiftUI
+
+@main
+struct ListView_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
